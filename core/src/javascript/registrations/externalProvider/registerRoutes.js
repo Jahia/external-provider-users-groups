9d13995634e5a9125eb94346c160abd647ebedfe
alex <alexandrk_@hotmail.com>
2020-02-07 13:47:56 -0500

    BACKLOG-12211 Update registration mechanism

--- core/src/javascript/registrations/externalProvider/registerRoutes.js
+++ core/src/javascript/registrations/externalProvider/registerRoutes.js
@@ -1,19 +1,19 @@
 import {registry} from '@jahia/ui-extender';
 
-export const registerRoutes = function (t) {
+export const registerRoutes = function () {
     const level = 'server';
     const parentTarget = 'administration-server';
 
     const path = '/administration/manageUserGroupProviders';
     const route = 'manageUserGroupProviders';
-    registry.addOrReplace('adminRoute', `${level}-${path.toLowerCase()}`, {
+    registry.add('adminRoute', `${level}-${path.toLowerCase()}`, {
         id: route,
         targets: [`${parentTarget}-usersandroles:3`],
         path: path,
         route: route,
         defaultPath: path,
         icon: null,
-        label: t('externalProvider.label'),
+        label: 'external-provider-users-groups:externalProvider.label',
         childrenTarget: 'usersandroles',
         isSelectable: true,
         level: level
--- core/src/javascript/registrations/index.js
+++ core/src/javascript/registrations/index.js
@@ -1,10 +1,5 @@
 import {registerRoutes as registerExternalProviderRoutes} from './externalProvider/registerRoutes';
-import {useTranslation} from 'react-i18next';
 
 export default function () {
-    const {t} = useTranslation('external-provider-users-groups');
-
-    registerExternalProviderRoutes(t);
-
-    return null;
+    registerExternalProviderRoutes();
 }
