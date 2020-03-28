0c222d691934758b14aa81f2a0bf209e95c2cf55
Francois Pral <fpral@jahia.com>
2020-03-28 13:12:13 +0100

    BACKLOG-12935: reorerding of the server settings

--- core/src/javascript/registrations/externalProvider/registerRoutes.js
+++ core/src/javascript/registrations/externalProvider/registerRoutes.js
@@ -2,7 +2,7 @@ import {registry} from '@jahia/ui-extender';
 
 export const registerRoutes = function () {
     registry.add('adminRoute', 'manageUserGroupProviders', {
-        targets: ['administration-server-usersAndRoles:3'],
+        targets: ['administration-server-usersAndRoles:60'],
         requiredPermission: 'adminUsers',
         icon: null,
         label: 'external-provider-users-groups:externalProvider.label',
