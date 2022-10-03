67ebb3fdaee222d106ef205688b6f1d9ad5bc371
Thomas Draier <tdraier@jahia.com>
2022-10-03 11:53:32 +0200

    BACKLOG-17240: upgrade libs, convert to webpack

--- core/src/javascript/index.js
+++ core/src/javascript/index.js
@@ -1,12 +1,6 @@
-import registrations from './registrations';
-import {registry} from '@jahia/ui-extender';
-import i18next from 'i18next';
-
-registry.add('callback', 'external-provider-users-groups', {
-    targets: ['jahiaApp-init:50'],
-    callback: async () => {
-        await i18next.loadNamespaces('external-provider-users-groups');
-        registrations();
-        console.log('%c External Provider Users Groups routes have been registered', 'color: #3c8cba');
-    }
+// Used only if jahia-ui-root is the host, experimental
+import('@jahia/app-shell/bootstrap').then(res => {
+    console.log(res);
+    window.jahia = res;
+    res.startAppShell(window.appShell.remotes, window.appShell.targetId);
 });
