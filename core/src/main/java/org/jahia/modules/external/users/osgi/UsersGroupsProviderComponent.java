0b1f3128813907fea671b4baf620b90e7833b0cd
David Griffon <dgriffon@jahia.com>
2024-04-04 11:36:26 +0200

    BACKLOG-22443: replace blueprint EDP check by annotated class to fix context refresh. (#69)

---
+++ core/src/main/java/org/jahia/modules/external/users/osgi/UsersGroupsProviderComponent.java
@@ -0,0 +1,76 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2024 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.external.users.osgi;
+
+import org.jahia.modules.external.ExternalProviderInitializerService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This OSGi service to ensure external provider spring context available before starting this module
+ */
+@Component(immediate = true, service = UsersGroupsProviderComponent.class)
+public class UsersGroupsProviderComponent {
+    private static final Logger logger = LoggerFactory.getLogger(UsersGroupsProviderComponent.class);
+
+    /**
+     * Not used reference to ensure the reference is available before starting this module.
+     */
+
+    private ExternalProviderInitializerService externalProviderInitializerService;
+
+    @Reference
+    public void setExternalProviderInitializerService(ExternalProviderInitializerService externalProviderInitializerService) {
+        this.externalProviderInitializerService = externalProviderInitializerService;
+    }
+
+    @Activate
+    private void activate() {
+        logger.info("ExternalProviderInitializerService {} Available", externalProviderInitializerService.toString());
+    }
+
+}
